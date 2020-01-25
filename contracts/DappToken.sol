dd6919493a7b460963fde36a3502e0649a15a46e
aashutosh227 <aashutoshsingh126@gmail.com>
2020-01-25 15:01:46 +0530

    Approve function test commit

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -7,10 +7,17 @@ contract DappToken  {
     string public symbol = "DAPP";
     string public standard = "Dapp Token v1.0";
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(
         address indexed _from,
         address indexed _to,
+        uint256 _value
+    );
+
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
         uint256 _value
     );
 
@@ -30,6 +37,14 @@ contract DappToken  {
         //Transfer Event
         emit Transfer(msg.sender,_to,_value);
 
+        return true;
+    }
+
+    //Approve
+    function approve(address _spender, uint256 _value) public payable returns(bool success){
+
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender,_spender,_value);
         return true;
     }
 }
